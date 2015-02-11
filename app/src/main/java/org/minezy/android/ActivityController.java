570c31310d40cf8904fd9534c80f92eeaa158265
Pawel Piech <pawel.piech@lookout.com>
2015-02-11 08:25:36 -0800

    Converted ContactsActivityController to an interface.

--- app/src/main/java/org/minezy/android/ActivityController.java
+++ app/src/main/java/org/minezy/android/ActivityController.java
@@ -3,7 +3,7 @@ package org.minezy.android;
 import android.content.Context;
 import android.content.Intent;
 
-public interface IActivityController {
+public interface ActivityController {
 
     public Context getContext();
 
